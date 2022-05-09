f8190970d6b7d744f35fccea7c41036c6a5a7661
Nemesis <zecaivano@gmail.com>
2022-05-09 17:09:51 -0300

    Projetos-Curso-Javscript

---
+++ Testes.js
@@ -0,0 +1,15 @@
+let minhaVar;
+let varNull = null;
+let zero = 0;
+
+// console.log(minhaVar);
+// console.log(varNull);
+// console.log (typeof minhaVar);
+// console.log (typeof varNull);
+
+// console.log (varNull == zero);
+
+const numero = 456;
+const numeroString = "456";
+
+console.log (numero == numeroString)
